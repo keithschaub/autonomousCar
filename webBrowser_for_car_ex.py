0927639799a622b368b30f1be4ba27bf74589918
keithschaub <37226559+keithschaub@users.noreply.github.com>
2024-02-04 17:00:53 -0600

    Add files via upload

--- webBrowser_for_car_ex.py
+++ webBrowser_for_car_ex.py
@@ -8,16 +8,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+
 def random_data_emitter():
+    angle = 30  # Start angle
     while True:
-        time.sleep(1)  # Simulating data receiving interval
-        angle = random.randint(0, 360)
-        distance = random.randint(1, 100)
+        time.sleep(0.1)  # Adjust the sleep time as needed
+        distance = 50 + random.randint(-5, 5)  # Distance around 50 cm with slight randomness
+        #print(f"Emitting angle: {angle}, distance: {distance}")
         socketio.emit('newdata', {'angle': angle, 'distance': distance})
+
+        angle += 5  # Increment angle
+        if angle > 150:
+            angle = 30  # Reset angle after reaching 150 degrees
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     threading.Thread(target=random_data_emitter).start()

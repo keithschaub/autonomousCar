69a503b4220ab4fecf131593a1a1f49167dcdd95
alumster <158204465+alumster@users.noreply.github.com>
2024-02-06 16:54:29 -0700

    Add files via upload
    * continuous run until object detected
    * waitForAck added
    * car calibration for straight line path and sonar boresight.
    * car modes: continual run until object detected, right turn, left turn, backward and scan sonar.

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -1,14 +1,7 @@
 /******************************************************************************
-TestRun.ino
-TB6612FNG H-Bridge Motor Driver Example code
-Michelle @ SparkFun Electronics
-8/20/16
-https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_Library
+MotorControl.ino
 
-Uses 2 motors to show examples of the functions in the library.  This causes
-a robot to do a little 'jig'.  Each movement has an equal and opposite movement
-so assuming your motors are balanced the bot should end up at the same place it
-started.
+https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_Library
 
 Resources:
 TB6612 SparkFun Library
@@ -18,8 +11,6 @@ Developed on Arduino 1.6.4
 Developed with ROB-9457
 ******************************************************************************/
 
-// This is the library for the TB6612 that contains the class Motor and all the
-// functions
 #include <SoftwareSerial.h>
 #include <Servo.h>
 #include <NewPing.h>
@@ -35,7 +26,7 @@ Developed with ROB-9457
 #define AIN2 9   // 4 no idea (9 works, but maybe this isn't used - check and delete later)
 #define BIN2 10  // 8 no idea (10 works, but  maybe this isn't used - check and delete later)
 #define PWMA 5
-#define PWMB 6
+#define PWMB 6  // PWM values controls balance
 #define STBY 3  // 9
 
 // these constants are used to allow you to make your motor configuration
@@ -49,23 +40,48 @@ const int offsetB = 1;
 // call the function more than once.
 Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+//Use of the drive function which takes as arguements the speed
+//and optional duration.  A negative speed will cause it to go
+//backwards.  Speed can be from -255 to 255.  Also use of the
+//brake function which takes no arguements.
+
+//   motor1.drive(255,1000);
+//   motor1.drive(-255,1000);
+//   motor1.brake();
+//   delay(1000);
+void forward(Motor motor1, int speed1, Motor motor2, int speed2)  //adjusts the PWM duty cycle of each motor independently for straightline calibration.
+{
+  motor1.drive(speed1);
+  motor2.drive(speed2);
+}
 
 // Radar/Sonar setup
 Servo myservo;
-#define SERVO_PIN 10  // sets the servo control to pin 10
+#define SERVO_PIN 10                   // sets the servo control to pin 10
 const int trigPin = 13;                // Trigger Pin connected to A13
 const int echoPin = 12;                // Echo Pin connected to A12
 const unsigned int maxDistance = 200;  // Maximum distance to measure
-int angle = 0;
+const int boresightOffset = 5;
 NewPing sonar(trigPin, echoPin, maxDistance);  // NewPing setup of pins and maximum distance.
-unsigned int sweepAndMeasure(int angle) {
-  myservo.write(angle);                     // radar to angle
-  delay(60);                                // Short delay to allow the servo to reach the position
-  unsigned int distance = sonar.ping_cm();  // Measure distance in mm
-  //client.print(angle);
-  //client.print(", ");
-  //client.println(measuredDistance);
-  return distance;
+
+unsigned int sweepAndMeasure(bool sweepMode, WiFiClient& client) {
+  unsigned int measuredMaxDistance = 999;
+  if (sweepMode) {
+    for (int angle = 30; angle <= 150; angle += 20) {
+      myservo.write(angle + boresightOffset);                     // radar to angle
+      delay(50);                                // Short delay to allow the servo to reach the position
+      unsigned int distance = sonar.ping_cm();  // Measure distance in mm
+      if (distance < measuredMaxDistance) measuredMaxDistance = distance;
+      client.print(angle);
+      client.print(", ");
+      client.println(distance);
+    }
+  } else {
+    myservo.write(90 + boresightOffset);
+    delay(20);
+    measuredMaxDistance = sonar.ping_cm();
+  }
+  return measuredMaxDistance;
 }
 
 //Wifi - Replace with your network credentials
@@ -74,28 +90,43 @@ const char* password = "401Cordoba";
 WiFiServer server(80);
 WiFiClient client;  // Declare WiFiClient object globally
 
-//keyboard interrupt
+// Keyboard interrupt
 int keyInt = 0;
+
+const int threshold = 35; // detection threshold (mm)
+bool ackReceived = false;  // Flag to indicate whether acknowledgment is received
+
 void handleClientInput() {
   if (client.available()) {
     char c = client.read();
-    if (c == 'A') keyInt = 86;
-    if (c == '8') keyInt += 50;
-    if (c == '2') keyInt -= 50;
-    if (keyInt < 0) keyInt = 0;
-    if (c == 'F') keyInt = 888;
-    if (c == 'B') keyInt = 222;
-    if (c == 'L') keyInt = 444;
-    if (c == 'R') keyInt = 666;
+    if (c == 'X') {
+      keyInt = 86;
+      ackReceived = true;  // Set the acknowledgment flag
+    } else if (c == '8' || c == '2' || c == 'F' || c == 'B' || c == 'L' || c == 'R' || c == 'S') {
+      keyInt += (c == '8') ? 50 : (c == '2') ? -50
+                                : (c == 'F') ? 888
+                                : (c == 'B') ? 222
+                                : (c == 'L') ? 444
+                                : (c == 'S') ? 555
+                                             : 666;
+      ackReceived = true;  // Set the acknowledgment flag
+    }
     //Serial.print("from PC: ");
     //Serial.println(keyInt);
   }
 }
 
+void waitForAck() {
+  while (!ackReceived) {
+    handleClientInput();  // Check for acknowledgment
+    delay(10);
+  }
+  ackReceived = false;  // Reset acknowledgment flag for the next command
+}
+
 void setup() {
   Serial.begin(9600);
   myservo.attach(SERVO_PIN);
-  Serial.print("====================\n");
   //Serial.println(ssid);
   WiFi.begin(ssid, password);
 
@@ -105,46 +136,37 @@ void setup() {
   }
   Serial.print("Connected to ");
   server.begin();
-  delay(1000);
-  Serial.print(WiFi.localIP());
+  delay(500);
+  //Serial.print(WiFi.localIP());
   Serial.println("...Start python script.");
 }
 
 void loop() {
-  //Use of the drive function which takes as arguements the speed
-  //and optional duration.  A negative speed will cause it to go
-  //backwards.  Speed can be from -255 to 255.  Also use of the
-  //brake function which takes no arguements.
-
-  //   motor1.drive(255,1000);
-  //   motor1.drive(-255,1000);
-  //   motor1.brake();
-  //   delay(1000);
 
   client = server.available();  // Move this line to the beginning of the loop
   //Serial.println("Client connected");
   while (client.connected()) {
     handleClientInput();
-    if (keyInt == 86) break;
+    if (keyInt == 86) break;  //Terminate from ESC key
 
-    if (keyInt == 888) {
-      unsigned int measuredMaxDistance = 999;
-      for (angle = 75; angle <= 105; angle += 15) {
-        unsigned int measuredDistance = sweepAndMeasure(angle);
-        if (measuredDistance < measuredMaxDistance) measuredMaxDistance = measuredDistance;
+    if (keyInt == 888) {            // Move forward continuously until Distance <= 35
+      unsigned int Distance = 999;  // Initialize Distance to a large value
+      while (Distance > threshold) {  // compare against detection threshold
+        Distance = sweepAndMeasure(false, client);
+        forward(motor1, 94, motor2, 100);
+        delay(50);  // Adjust this delay as needed to control the poll frequency
       }
-      //unsigned int measuredDistance = sweepAndMeasure(angle);
-      if (measuredMaxDistance <= 30) brake(motor1, motor2);
-      else forward(motor1, motor2, 200);
-      delay(400);
+      brake(motor1, motor2);
+      waitForAck();  // Uncomment if acknowledgment is required after each forward movement
     }
 
-    //Use of the back/forward function, which takes as arguments two motors
+    //Use of the forward function, which takes as arguments two motors
     //and optionally a speed.  Either a positive number or a negative
     //number for speed will cause it to go backwards
     if (keyInt == 222) {
-      back(motor1, motor2, -200);
+      forward(motor1, -185, motor2, -200);  //backward movement since speed param is negative
       delay(1000);
+      waitForAck();
     }
 
     //Use of the left and right functions which take as arguements two
@@ -152,19 +174,27 @@ void loop() {
     //the appropriate direction.  For turning a single motor use drive.
     if (keyInt == 666) {
       left(motor1, motor2, 200);
-      delay(800);
+      delay(400);
+      waitForAck();
     }
 
     if (keyInt == 444) {
       right(motor1, motor2, -200);
-      delay(800);
+      delay(400);
+      waitForAck();
     }
 
     //Use of the brake function which takes as arguments two motors.
     //Note that functions do not stop motors on their own.
-    brake(motor1, motor2);
-    delay(500);
-    keyInt = 0;  //remove for continuous run
+
+    if (keyInt == 555) {
+      brake(motor1, motor2);
+      unsigned int distance = sweepAndMeasure(true, client);
+      waitForAck();
+    }
+    brake(motor1, motor2); //remove for continuous run of last command
+    keyInt = 0; //reset keyInt for next client command
   }
+  unsigned int distance = sweepAndMeasure(false, client); //re-center sonar
   client.stop();
 }
